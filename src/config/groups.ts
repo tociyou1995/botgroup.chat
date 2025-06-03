eb872fd79120c062fb5d4050a2d719b67a2a6d0d
tociyou1995 <150312743+tociyou1995@users.noreply.github.com>
2025-06-03 14:47:26 +0800

    Update groups.ts

--- src/config/groups.ts
+++ src/config/groups.ts
@@ -8,13 +8,13 @@ export interface Group {
 }
 
 export const groups: Group[] = [
-  {
+  /*{
     id: 'group1',
     name: '🔥硅碳生命体交流群',
     description: '群消息关注度权重：“user”的最新消息>其他成员最新消息>“user”的历史消息>其他成员历史消息>',
     members: [ 'ai8',  'ai6', 'ai7', 'ai9', 'ai10', 'ai5'],
     isGroupDiscussionMode: false
-  },
+  },*/
   /*
   {
     id: 'group2',
@@ -30,13 +30,13 @@ export const groups: Group[] = [
     isGroupDiscussionMode: true,
     members: [ 'ai8', 'ai6',  'ai9', 'ai10'],
   },*/
-  {
+ /* {
     id: 'group4',
     name: '👨‍👩‍👧‍👦豆氏家族',
     description: '群员和关系介绍：豆包和豆沙是夫妻（刚结婚），豆孩是豆包和豆沙的孩子（婚前生的），豆爸和豆妈是豆包的父母，豆奶和豆爷是豆包的爷爷奶奶，豆姐和豆妹是豆包的姐姐妹妹。注意：明确自己身份和成员关系，说话风格要符合自己的身份。',
     isGroupDiscussionMode: false,
     members: [ 'ai5', 'ai11', 'ai12', 'ai13', 'ai14', 'ai15', 'ai16', 'ai17', 'ai18'],
-  },
+  },*/
   /*
   {
     id: 'group4',
@@ -45,4 +45,11 @@ export const groups: Group[] = [
     isGroupDiscussionMode: true,
     members: [ 'ai1', 'ai5', 'ai6',  'ai9', 'ai10'],
   }*/
+  {
+    id: 'group5',
+    name: '硕士论文沟通交流群',
+    description: '群员和关系介绍：用户是提出命题的人；研究生的任务是根据用户要求、导师建议完成论文研究框架设计、研究实施、研究改进和论文撰写，对于导师和用户的要求，他每次都要有所回应；导师需要对研究生每次回答的对应内容进行评价、按照答辩要求提出问题、给出解决问题的建议，并且按照用户的指令，对研究生下一步工作做出安排、规划和指导。',
+    isGroupDiscussionMode: false,
+    members: [ 'a19', 'ai20'],
+  }
 ];
